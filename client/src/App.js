4bb8ca4fcf96d823bb16c904978858e5dcb807a4
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-11-03 21:16:25 +0300

    -feat- Sidebar mostly done added some input and buttons also the whole css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { StreamChat } from "stream-chat";
-import { Chat } from "stream-chat-react";
+import { ChannelList, Chat } from "stream-chat-react";
 import { Cookies } from "universal-cookie";
 
-import { ChannelContainer, ChannelListContainer } from "./components";
+import {
+  ChannelContainer,
+  ChannelListContainer,
+  TeamChannelList,
+} from "./components";
+
+import "./App.css";
 
 const apiKey = `hy7x4t93r53t`;
 
@@ -16,6 +22,11 @@ const App = () => {
         <Chat client={client} theme="team-light">
           <ChannelListContainer />
           <ChannelContainer />
+          <ChannelList
+            filters={{}}
+            channelRenderFilterFn={() => {}}
+            List={(listProps) => <TeamChannelList {...listProps} />}
+          />
         </Chat>
       </div>
     </div>
---
+++ client/src/components/TeamChannelPreview.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeamChannelPreview = () => {
+  return <div></div>;
+};
+
+export default TeamChannelPreview;
