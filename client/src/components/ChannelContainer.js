d09023fa368508d87bee6f7a1a520be0669be109
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-11-05 16:15:57 +0300

    -feat- Now can send custom messages and emojis etc.

--- client/src/components/ChannelContainer.js
+++ client/src/components/ChannelContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Channel, useChatContext } from "stream-chat-react";
+import { Channel, useChatContext, MessageTeam } from "stream-chat-react";
 
-import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from "./";
+import { ChannelInner, CreateChannel, EditChannel } from "./";
 
 const ChannelContainer = ({
   isCreating,
@@ -42,7 +42,7 @@ const ChannelContainer = ({
     <div className="channel__container">
       <Channel
         EmptyStateIndicator={EmptyState}
-        Message={(messageProps, i) => <TeamMessage key={i} {...messageProps} />}
+        Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />}
       >
         <ChannelInner setIsEditing={setIsEditing} />
       </Channel>
